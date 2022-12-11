8b832a792e696c27fc45a0ba60904154fed101a3
trqminh <minhtranquangcr@gmail.com>
2022-12-11 00:21:15 -0600

    chore: configs and scripts update

--- scripts/vis_data.sh
+++ scripts/vis_data.sh
@@ -1,11 +1,12 @@
 export AISTRON_DATASETS=../data/datasets/
 
 config_file=configs/Base-RCNN-FPN-KINS2020.yaml
-output_dir=../data/outtest/aistron_vis_gt_kins2020/
+output_dir=../data/outtest/aistron_vis_gt_test_kins2020/
 mkdir ${output_dir}
 
 
 python visualize_data.py --config-file ${config_file} \
     --output-dir ${output_dir} \
     --source "annotation" \
-    --split "train"
+    --split "test" \
+    --option "visible"
