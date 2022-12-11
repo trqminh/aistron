89d9c20ffd105b1e686b32b25fef22f37d8cfc54
trqminh <minhtranquangcr@gmail.com>
2022-12-11 00:20:43 -0600

    feat: argparse for visualize options

--- visualize_data.py
+++ visualize_data.py
@@ -37,6 +37,7 @@ def parse_args(in_args=None):
     parser.add_argument("--output-dir", default="./", help="path to output directory")
     parser.add_argument("--show", action="store_true", help="show output in a window")
     parser.add_argument("--split", default="train", help="data split to visualize")
+    parser.add_argument("--option", default="amodal", help="opt for visualization types (e.g. amodal, visible,..)")
     parser.add_argument(
         "opts",
         help="Modify config options using the command-line",
@@ -98,5 +99,5 @@ if __name__ == "__main__":
         for dic in tqdm.tqdm(dicts):
             img = utils.read_image(dic["file_name"], "RGB")
             visualizer = AmodalVisualizer(img, metadata=metadata, scale=scale)
-            vis = visualizer.draw_dataset_dict(dic, option='visible')
+            vis = visualizer.draw_dataset_dict(dic, option=args.option)
             output(vis, os.path.basename(dic["file_name"]))
