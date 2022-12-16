3f00dd7a73e5c6f4de94c0849a7bc42de76754c1
trqminh <minhtranquangcr@gmail.com>
2022-12-16 14:36:04 -0600

    fix: rename evaluator type for coco amodal datasets

--- aistron/data/datasets/coco_amodal.py
+++ aistron/data/datasets/coco_amodal.py
@@ -251,7 +251,7 @@ def register_aistron_cocolike_instances(name, metadata, json_file, image_root):
     # 2. Optionally, add metadata about this dataset,
     # since they might be useful in evaluation, visualization or logging
     MetadataCatalog.get(name).set(
-        json_file=json_file, image_root=image_root, evaluator_type="coco", **metadata
+        json_file=json_file, image_root=image_root, evaluator_type="coco_amodal", **metadata
     )
 
 
