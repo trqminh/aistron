cc0c43aa78a2874bf9485591d228659ce83697da
trqminh <minhtranquangcr@gmail.com>
2023-03-10 20:19:40 -0600

    feat: add setup.py to use aistron as 3rd library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='aistron',
+      version='0.0.1',
+      author='Minh Tran',
+      packages=['aistron'],
+      description='An AIS library',
+      license='MIT',
+      install_requires=[
+            'torch',
+      ],
+)
