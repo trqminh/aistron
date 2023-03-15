af8e0ae888ea00a89f1045d53a0fd1fac1248a06
trqminh <minhtranquangcr@gmail.com>
2023-03-14 19:32:12 -0500

    feat: add inference forward func for aisformer

--- aistron/modeling/roi_heads/mask_heads/aisformer/aisformer.py
+++ aistron/modeling/roi_heads/mask_heads/aisformer/aisformer.py
@@ -20,6 +20,8 @@ from .transformer import (
 from detectron2.config import configurable
 from detectron2.structures import Instances
 from detectron2.modeling.roi_heads.mask_head import ROI_MASK_HEAD_REGISTRY
+from detectron2.layers.wrappers import move_device_like
+
 
 __all__= ["AISFormer"]
 
@@ -192,7 +194,54 @@ class AISFormer(nn.Module):
 
         mask_loss = F.binary_cross_entropy_with_logits(pred_mask_logits, gt_masks, reduction="mean")
         return mask_loss
- 
+
+    @staticmethod
+    def mask_inference(pred_mask_logits: torch.Tensor, 
+                       pred_instances: List[Instances],
+                       pred_mask_type=None):
+        """
+        Inherit from detectron2 mask_rcnn_inference
+        Convert pred_mask_logits to estimated foreground probability masks while also
+        extracting only the masks for the predicted classes in pred_instances. For each
+        predicted box, the mask of the same class is attached to the instance by adding a
+        new "pred_masks" field to pred_instances.
+        Args:
+            pred_mask_logits (Tensor): A tensor of shape (B, C, Hmask, Wmask) or (B, 1, Hmask, Wmask)
+                for class-specific or class-agnostic, where B is the total number of predicted masks
+                in all images, C is the number of foreground classes, and Hmask, Wmask are the height
+                and width of the mask predictions. The values are logits.
+            pred_instances (list[Instances]): A list of N Instances, where N is the number of images
+                in the batch. Each Instances must have field "pred_classes".
+        Returns:
+            None. pred_instances will contain an extra "pred_masks" field storing a mask of size (Hmask,
+                Wmask) for predicted class. Note that the masks are returned as a soft (non-quantized)
+                masks the resolution predicted by the network; post-processing steps, such as resizing
+                the predicted masks to the original image resolution and/or binarizing them, is left
+                to the caller.
+        """
+        cls_agnostic_mask = pred_mask_logits.size(1) == 1
+
+        if cls_agnostic_mask:
+            mask_probs_pred = pred_mask_logits.sigmoid()
+        else:
+            # Select masks corresponding to the predicted classes
+            num_masks = pred_mask_logits.shape[0]
+            class_pred = cat([i.pred_classes for i in pred_instances])
+            device = (
+                class_pred.device
+                if torch.jit.is_scripting()
+                else ("cpu" if torch.jit.is_tracing() else class_pred.device)
+            )
+            indices = move_device_like(torch.arange(num_masks, device=device), class_pred)
+            mask_probs_pred = pred_mask_logits[indices, class_pred][:, None].sigmoid()
+        # mask_probs_pred.shape: (B, 1, Hmask, Wmask)
+
+        num_boxes_per_image = [len(i) for i in pred_instances]
+        mask_probs_pred = mask_probs_pred.split(num_boxes_per_image, dim=0)
+
+        for prob, instances in zip(mask_probs_pred, pred_instances):
+            instances.set(pred_mask_type, prob)  # (1, Hmask, Wmask)
+
 
     def layers(self, x):
         x_ori = x.clone()
@@ -259,5 +308,8 @@ class AISFormer(nn.Module):
                 "loss_invisible_mask": loss_invisible_mask * 0.25
             }
         else:
-            ## TODO: Inference forward
-            pass
+            ## Inference forward
+            vi_masks, bo_masks, a_masks, invisible_masks = self.layers(x)
+            self.mask_inference(vi_masks, instances, 'pred_visible_masks')
+            self.mask_inference(a_masks, instances, 'pred_amodal_masks')
+            return instances
