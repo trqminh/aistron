419eeaca1f7c624461dacda3808be315bbb93322
trqminh <minhtranquangcr@gmail.com>
2023-06-05 13:47:11 -0500

    docs: update BCNet readme and training example

--- projects/BCNet/README.md
+++ projects/BCNet/README.md
@@ -31,24 +31,25 @@ python projects/BCNet/train_net.py --config-file projects/BCNet/path/to/config.y
 ### KINS2020 Dataset
 | Name | Backbone | epochs |AP|AR|Visible AP| Trained model |
 |-------|:---:|:-------:|:-------:|:-------:|:-------:|:-------:|
-|[BCNet]()|Resnet-101|~6|...|...|-|[model]()|
+|[BCNet](configs/KINS2020/bcnet_R50_FPN_kins2020_6ep_bs1.yaml)|Resnet-50|~6|...|...|-|[model]()|
+|[BCNet](configs/KINS2020/bcnet_R101_FPN_kins2020_6ep_bs1.yaml)|Resnet-101|~6|...|...|-|[model]()|
 
 
 ### D2SA Dataset
 | Name | Backbone | epochs |AP|AR|Visible AP| Trained model |
 |-------|:---:|:-------:|:-------:|:-------:|:-------:|:-------:|
-|[BCNet]()|Resnet-50|~18|...|...|-|[model]()|
+|[BCNet](configs/D2SA/bcnet_R50_FPN_d2sa_18ep_bs2.yaml)|Resnet-50|~18|...|...|-|[model]()|
 
 ### COCOA Dataset
 COCOA No stuff, no class dataset
 
 | Name | Backbone | epochs |AP|AR|Visible AP| Trained model |
 |-------|:---:|:-------:|:-------:|:-------:|:-------:|:-------:|
-|[BCNet]()|Resnet-101|~8|...|...|-|[model]()|
+|[BCNet](configs/COCOA/bcnet_R101_FPN_cocoa_8ep_bs2.yaml)|Resnet-101|~8|...|...|-|[model]()|
 
 ### COCOA-cls Dataset
 COCOA with classes
 
 | Name | Backbone | epochs |AP|AR|Visible AP| Trained model |
 |-------|:---:|:-------:|:-------:|:-------:|:-------:|:-------:|
-|[BCNet]()|Resnet-50|~8|...|...|-|[model]()|
+|[BCNet](configs/COCOA-cls/bcnet_R50_FPN_cocoa_cls_8ep_bs2.yaml)|Resnet-50|~8|...|...|-|[model]()|
--- projects/BCNet/train.sh
+++ projects/BCNet/train.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=2
 export AISTRON_DATASETS=../data/datasets/
 
-# python projects/BCNet/train_net.py --config-file projects/BCNet/configs/KINS2020/bcnet_R50_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 \
+#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/KINS2020/bcnet_R50_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 --resume \
 
-#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/KINS2020/bcnet_R101_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 \
+#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/KINS2020/bcnet_R101_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 --resume \
 
-#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/COCOA/bcnet_R101_FPN_cocoa_8ep_bs2.yaml --num-gpus 1 \
+#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/COCOA/bcnet_R101_FPN_cocoa_8ep_bs2.yaml --num-gpus 1 --resume \
 
-#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/COCOA-cls/bcnet_R50_FPN_cocoa_cls_8ep_bs2.yaml --num-gpus 1 \
+#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/COCOA-cls/bcnet_R50_FPN_cocoa_cls_8ep_bs2.yaml --num-gpus 1 --resume \
 
-#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/D2SA/bcnet_R50_FPN_d2sa_18ep_bs2.yaml --num-gpus 1 \
+#python projects/BCNet/train_net.py --config-file projects/BCNet/configs/D2SA/bcnet_R50_FPN_d2sa_18ep_bs2.yaml --num-gpus 1 --resume \
