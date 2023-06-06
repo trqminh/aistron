0a3eaa086ca44266806dbca935a218632cdba012
trqminh <minhtranquangcr@gmail.com>
2023-06-06 15:03:24 -0500

    chore: clean up configs and scripts for projects

--- projects/AISFormer/train.sh
+++ projects/AISFormer/train.sh
@@ -8,3 +8,5 @@ export AISTRON_DATASETS=../data/datasets/
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/COCOA/aisformer_R101_FPN_cocoa_8ep_bs2.yaml --num-gpus 1 \
 
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/COCOA-cls/aisformer_R50_FPN_cocoa_cls_8ep_bs2.yaml --num-gpus 1 \
+
+#python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/D2SA/aisformer_R50_FPN_d2sa_18ep_bs2.yaml --num-gpus 1 \
