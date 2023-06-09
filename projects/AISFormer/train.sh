8de27b191873c57515bf4d21df05915316d4a323
trqminh <minhtranquangcr@gmail.com>
2023-06-09 13:21:14 -0500

    feat: add more configs for projects and train updates

--- projects/AISFormer/train.sh
+++ projects/AISFormer/train.sh
@@ -1,12 +1,16 @@
 export CUDA_VISIBLE_DEVICES=2
 export AISTRON_DATASETS=../data/datasets/
 
+# KINS
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/KINS2020/aisformer_R50_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 \
-
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/KINS2020/aisformer_R101_FPN_kins2020_6ep_bs1.yaml --num-gpus 1 \
 
+# COCOA
+#python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/COCOA/aisformer_R50_FPN_cocoa_8ep_bs2.yaml --num-gpus 1 \
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/COCOA/aisformer_R101_FPN_cocoa_8ep_bs2.yaml --num-gpus 1 \
 
+# COCOA-cls
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/COCOA-cls/aisformer_R50_FPN_cocoa_cls_8ep_bs2.yaml --num-gpus 1 \
 
+# D2SA
 #python projects/AISFormer/train_net.py --config-file projects/AISFormer/configs/D2SA/aisformer_R50_FPN_d2sa_18ep_bs2.yaml --num-gpus 1 \
