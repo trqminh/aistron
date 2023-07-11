41a410be7fc1ebd635f0f3d4b30da86d51b3995c
trqminh <minhtranquangcr@gmail.com>
2023-07-10 23:46:37 -0500

    fix: add setup.py with finding project package

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+PROJECTS = {
+    "aistron.projects.aisformer": "projects/AISFormer/aisformer/",
+    "aistron.projects.bcnet": "projects/BCNet/bcnet/",
+}
 setup(name='aistron',
       version='0.1.0',
       author='Minh Tran',
-      packages=find_packages(),
+      packages=find_packages() + list(PROJECTS.keys()),
+      package_dir=PROJECTS,
       description='An AIS library',
       license='Apache License 2.0',
       install_requires=[
